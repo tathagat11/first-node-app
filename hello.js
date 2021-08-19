8f1bf2e324d2b8a22345dceeb5a21a8b16976bc5
tathagat11 <tathagat10.2k@gmail.com>
2021-08-19 16:50:29 +0530

    idk

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-//const PORT =process.env.PORT || 5000
+const PORT =process.env.PORT || 5000
 
 http.createServer(function (req,res){
 	var q = url.parse(req.url, true);
@@ -21,6 +21,6 @@ http.createServer(function (req,res){
 		console.log('request incoming...'+req.url);
 		return res.end();
 	});
-}).listen(8080);
+}).listen(PORT);
 
 console.log('port=8080');
